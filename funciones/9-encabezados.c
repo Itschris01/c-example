150803d7c990b0f1772e880cc6098ea58b34ee6c
Cristhian <cristhian.rojasalvarez@ucr.ac.cr>
2024-04-28 23:23:37 -0600

    Añadiendo funcion 5

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -2,17 +2,20 @@
 #include "9-encabezados.h"
 
 int main() {
-
+    // Declaración de la variable
+    int resultadoF5;
     int v;
 
     // Llama a las 3 funciones
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
-
+    //LLamado de función
+    resultadoF5 = funcion5(12);
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
-
+    // Mostrar en pantalla
+    printf("resultadoF5 = %d\n", resultadoF5);
     return 0;
 
 }
@@ -30,8 +33,14 @@ void funcion2(int a, float b) {
 
 // funcion3(): Imprime un mensaje. Retorna el valor que recibió como argumento
 // incrementado en 2
+// 
 int funcion3(int x) {
     printf("En funcion3()...\n");
     int y = x + 2;
+    return y;
+}
+int funcion5(int x) {
+    printf("En funcion5()...\n");
+    int y = x + 21;
     return y;
 }
