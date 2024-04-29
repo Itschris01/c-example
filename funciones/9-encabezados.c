a8463f4ad6204c92c3fed630d1f0166ee718e35c
Cristhian <cristhian.rojasalvarez@ucr.ac.cr>
2024-04-28 23:45:28 -0600

    Resolviendo mensajes de conflicto

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -2,26 +2,21 @@
 #include "9-encabezados.h"
 
 int main() {
-<<<<<<< HEAD
     // Declarando variable
     int resultadoF4;
-=======
     // Declaración de la variable
     int resultadoF5;
->>>>>>> feature-branch
     int v;
 
     // Llama a las 3 funciones
     funcion1();
     funcion2(5, 2.67);
-<<<<<<< HEAD
     v = funcion3(10); 
     // Llamado a la funcion
     resultadoF4 =funcion4(7);
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
     printf("resultadoF4 = %d\n", resultadoF4);
-=======
     v = funcion3(10);
     //LLamado de función
     resultadoF5 = funcion5(12);
@@ -29,7 +24,6 @@ int main() {
     printf("v = %d\n", v);
     // Mostrar en pantalla
     printf("resultadoF5 = %d\n", resultadoF5);
->>>>>>> feature-branch
     return 0;
 
 }
@@ -53,15 +47,12 @@ int funcion3(int x) {
     int y = x + 2;
     return y;
 }
-<<<<<<< HEAD
 // Implementacion de la funcion4
 int funcion4(int x) {
     printf("En funcion4()...\n");
-    int y = x - 2;
-=======
+    int y = x - 2;}
 int funcion5(int x) {
     printf("En funcion5()...\n");
     int y = x + 21;
->>>>>>> feature-branch
     return y;
 }
